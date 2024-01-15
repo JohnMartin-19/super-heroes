2bfb809db8dfa34ff0aa95bc269c35086c1a9114
John <johnnkonge2020@gmail.com>
2024-01-15 19:37:34 +0300

    added post method

--- python-code-challenge-superheroes/python-code-challenge-superheroes/code-challenge/app/app.py
+++ python-code-challenge-superheroes/python-code-challenge-superheroes/code-challenge/app/app.py
@@ -56,6 +56,40 @@ def Powers():
         405,
     ) 
 
+@app.route('/powers/id', methods = ['GET','PATCH','DELETE'])
+def get_power_by_id(id):
+    id = int(request.args.get('id'))
+    if request.method == 'GET':
+        powers = Powers.query.filter_by(id=id).first()
+    
+        return make_response(
+            jsonify([powers.to_dict() for power in powers ]), 200
+        )
+    elif request.method=='PATCH': #update
+        powers = Powers.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(powers, attr, request.form[attr])
+
+        db.session.add(powers)
+        db.session.commit()
+        powers_dict = powers.to_dict()
+        powers_dict["message"]="Power Updated Successfully!"
+        response =  make_response(jsonify(powers_dict),200)
+
+        return response
+        
+@app.route('/hero_powers', methods = ['POST'])
+def hero_powers():
+    if request.method == 'POST':
+        newHeroPower = hero_power_association(**request.json)
+        db.session.add(newHeroPower)
+        db.session.commit()
+
+        response = 'new hero association added'
+        return jsonify({'message':response}), 201
+    
+    
+    
 
 
 if __name__ == '__main__':
