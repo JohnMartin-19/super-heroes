1932891f743ae7593d608e33586fb0659108751b
John <johnnkonge2020@gmail.com>
2024-01-15 19:12:48 +0300

    get by id

--- python-code-challenge-superheroes/python-code-challenge-superheroes/code-challenge/app/app.py
+++ python-code-challenge-superheroes/python-code-challenge-superheroes/code-challenge/app/app.py
@@ -14,7 +14,7 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 @app.route('/heros', methods=['GET'])
-def heros():
+def heros(heros):
     if request.method == 'GET':
         heros = heros.query.all()
 
@@ -27,6 +27,18 @@ def heros():
         jsonify({"text": "Method Not Allowed"}),
         405,
     ) 
+
+
+@app.route('/heros/id', methods = ['GET'])
+def get_hero_by_id(id):
+    id = int(request.args.get('id'))
+    if request.method == 'GET':
+        results = heros.query.filter_by(id=id).first()
+    
+        return make_response(
+            jsonify([results.to_dict() for result in results ]), 200
+        )
+
 
 if __name__ == '__main__':
     app.run(port=5555)
